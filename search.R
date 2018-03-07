8dcd8df89cee7eee98d5ef198c08cca6127132a1
wtawill9 <wtawill9@uw.edu>
2018-03-06 20:36:52 -0800

    draft

--- search.R
+++ search.R
@@ -1,6 +1,7 @@
 library("httr")
 library("jsonlite")
 library("dplyr")
+library("tidyverse")
 
 # Function for finding the json data by taking the query parameter to find the results
 PwnSearch <- function(service) {
@@ -16,12 +17,6 @@ breaches.data <- breaches$DataClasses
 
 
 breaches <- PwnSearch("breaches") %>%
-  select(Title, BreachDate, AddedDate, ModifiedDate, PwnCount, Description)
+  select(Title, ModifiedDate, PwnCount, Description)
 
 category <- PwnSearch("dataclasses")
-
-names(breaches.data) <- breaches$Title 
-breach.name <- names(breaches.data[grepl(category[1], breaches.data)])
-expo.breach <- breaches %>%
-  filter(Title %in% breach.name)
-expo.breach
